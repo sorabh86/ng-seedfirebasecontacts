126d790a81530b8a85bf95a60cd49a9e1d42bf00
Sorabh <ssorabh.ssharma@gmail.com>
2017-09-30 16:33:40 +0530

    initialize and code

---
+++ README.md
@@ -0,0 +1,54 @@
+# Overview 
+This Project is a simple Contact Management Application. 
+
+It is a full CRUD Application using AngularJS and Firebase cloud database. Users can add, edit, and delete contacts using FirebaseAPI. 
+
+Main goal is to obtain experience on angularjs application & website development with firebase services.
+
+Project contains Angular Seed boiler plate, angularfire, firebase.
+
+## Contributions
+* [`angular-seed`](https://github.com/angular/angular-seed)
+* `angularjs`
+* [firebase](https://firebase.google.com/)
+* [angularfire](https://github.com/firebase/angularfire)
+* bootstap 3
+* jquery
+
+## Experience in full SDLC
+* AngularJS (1.0, 2.0) Apps
+* PHP webstites
+* JavaScript (ECMA Script 5|6)
+* Flash | Flex | flexsdk
+* Frameworks | Wordpress | Magento 
+* Mobile (Android | Window | IPHONE), Desktop Application
+* Databases (Relational Databases | MySQL, NoSQL | MongoDB )
+## `Contact Us`
+SKYPE : ssorabh.ssharma
+
+email : ssorabh.ssharma@hotmail.com
+
+github : https://github.com/sorabh86
+
+-----------
+
+© copyright to expertcodedesign.com on 2016 
+
+-----------
+
+### Install Dependencies
+
+```
+npm install
+```
+
+```
+bower install
+```
+### Run the Application
+
+```
+npm start
+```
+Now browse to the app at [`localhost:8000/index.html`][local-app-url].
+
---
+++ app/contacts/contacts.js
@@ -0,0 +1,158 @@
+'use strict';
+angular.module('contactApp.contacts', ['ngRoute', 'firebase'])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/contacts', {
+      templateUrl: 'contacts/contacts.html',
+      controller: 'ContactsCtrl'
+    });
+}])
+
+.controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray){
+	// Init firebase
+	var ref = firebase.database().ref().child("contacts");
+	// Get contacts
+	$scope.contacts = $firebaseArray(ref);
+	
+	$scope.removeContact = function(c) {
+		
+	};
+	
+	$scope.showAddForm = function(){
+		$scope.addFormShow = true;
+	};
+	$scope.showEditForm = function(c) {
+		$scope.editFormShow = true;
+
+		$scope.name = c.name;
+		$scope.email = c.email;
+		$scope.company = c.company;
+		$scope.mobile_phone = c.phones ? c.phones[0].mobile : '';
+		$scope.home_phone = c.phones ? c.phones[0].home : '';
+		$scope.work_phone = c.phones ? c.phones[0].work : '';
+		$scope.street_address = c.address ? c.address[0].street_address : '';
+		$scope.city = c.address ? c.address[0].city : '';
+		$scope.state = c.address ? c.address[0].state : '';
+		$scope.zipcode = c.address ? c.address[0].zipcode : '';
+
+		$scope.id = c.$id;
+	};
+	
+	$scope.hide = function(){
+		$scope.addFormShow = false;
+		$scope.editFormShow = false;
+		$scope.contactShow = false;
+	};
+
+	// Submit contact
+	$scope.addFormSubmit = function(){
+		// Assign values
+		if($scope.name) var name = $scope.name; else var name = null;
+		if($scope.email) var email = $scope.email; else var email = null;
+		if($scope.company) var company = $scope.company; else var company = null;
+		if($scope.mobile_phone) var mobile_phone = $scope.mobile_phone; else var mobile_phone = null;
+		if($scope.home_phone) var home_phone = $scope.home_phone; else var home_phone = null;
+		if($scope.work_phone) var work_phone = $scope.work_phone; else var work_phone = null;
+		if($scope.street_address) var street_address = $scope.street_address; else var street_address = null;
+		if($scope.city) var city = $scope.city; else var city = null;
+		if($scope.state) var state = $scope.state; else var state = null;
+		if($scope.zipcode) var zipcode = $scope.zipcode; else var zipcode = null;
+
+		// Build & Send Object
+		$scope.contacts.$add({
+			name:name,
+			email:email,
+			company:company,
+			phones:[
+				{
+					mobile:mobile_phone,
+					home:home_phone,
+					work:work_phone
+				}
+			],
+			address:[
+				{
+					street_address:street_address,
+					city:city,
+					state:state,
+					zipcode:zipcode
+				}
+			]
+		}).then(function(ref){
+			// var id = ref.key();
+			// console.log('added : ID=',id);
+
+			//Clear Form
+			clearFields();
+
+			$scope.addFormShow = false;
+
+			// Show user a message
+			$scope.msg = "Contact Added!!!";
+		});
+	};
+
+	$scope.editFormSubmit = function() {
+		// Get id
+		var id = $scope.id;
+
+		// Get Record
+		var record = $scope.contacts.$getRecord(id);
+
+		// Assign Values
+		record.name = $scope.name;
+		record.email = $scope.email;
+		record.company = $scope.company;
+		record.phones[0].work = $scope.work_phone;
+		record.phones[0].home = $scope.home_phone;
+		record.phones[0].mobile = $scope.mobile_phone;
+		record.address[0].street_address = $scope.street_address;
+		record.address[0].city = $scope.city;
+		record.address[0].state = $scope.state;
+		record.address[0].zipcode = $scope.zipcode;
+
+		// Save Contact
+		$scope.contacts.$save(record).then(function(ref){
+			console.log(ref.key);
+		});
+
+		clearFields();
+
+		// Hide edit form
+		$scope.editFormShow = false;
+
+		$scope.msg = "Contact Updated!!!";
+	};
+
+	$scope.showContact = function(contact) {
+		$scope.name = contact.name;
+		$scope.email = contact.email;
+		$scope.company = contact.company;
+		$scope.mobile_phone = contact.phones ? contact.phones[0].mobile : '';
+		$scope.home_phone = contact.phones ? contact.phones[0].home : '';
+		$scope.work_phone = contact.phones ? contact.phones[0].work : '';
+		$scope.street_address = contact.address ? contact.address[0].street_address : '';
+		$scope.city = contact.address ? contact.address[0].city : '';
+		$scope.state = contact.address ? contact.address[0].state : '';
+		$scope.zipcode = contact.address ? contact.address[0].zipcode : '';
+
+		$scope.contactShow = true;
+	};
+
+	// clear $scope fields
+	function clearFields() {
+		$scope.id = '';
+		$scope.name = '';
+		$scope.email = '';
+		$scope.company = '';
+		$scope.mobile_phone = '';
+		$scope.home_phone = '';
+		$scope.work_phone = '';
+		$scope.street_address = '';
+		$scope.city = '';
+		$scope.state = '';
+		$scope.zipcode = '';
+	}
+
+}]);
